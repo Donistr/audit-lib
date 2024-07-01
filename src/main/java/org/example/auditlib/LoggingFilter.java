3eb28aa1097c0be7a545c47f8a355128f902e86f
Donistr <deniselag@mail.ru>
2024-07-01 10:38:54 +0400

    refactor: забыл запустить checkstyle, исправил оформление

--- src/main/java/org/example/auditlib/LoggingFilter.java
+++ src/main/java/org/example/auditlib/LoggingFilter.java
@@ -1,9 +1,13 @@
 package org.example.auditlib;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import jakarta.servlet.*;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.Filter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -15,7 +19,7 @@ public class LoggingFilter implements Filter {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(LoggingFilter.class);
 
-    private static final ObjectMapper objectMapper = new ObjectMapper();
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
@@ -27,7 +31,7 @@ public class LoggingFilter implements Filter {
 
         filterChain.doFilter(httpRequest, httpResponse);
 
-        String requestBody = objectMapper.writeValueAsString(objectMapper.readTree(httpRequest.getRequestBody()));
+        String requestBody = OBJECT_MAPPER.writeValueAsString(OBJECT_MAPPER.readTree(httpRequest.getRequestBody()));
         String responseBody = httpResponse.getResponseBody();
 
         StringBuilder stringBuilder = new StringBuilder()
